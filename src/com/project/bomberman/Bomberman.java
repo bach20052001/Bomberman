bed6cd6b8e6aa608ffa86d461c8c0dfca64ace21
dtthuyen <71447270+dtthuyen@users.noreply.github.com>
2020-11-29 12:33:24 +0700

    Update Bomberman.java

--- src/com/project/bomberman/Bomberman.java
+++ src/com/project/bomberman/Bomberman.java
@@ -1,12 +1,29 @@
 package com.project.bomberman;
 
-import com.project.bomberman.gui.Frame;
 
-public class Bomberman {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Bomberman extends Application {
+
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+        //Platform.setImplicitExit(true);
+        Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Bomberman");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 	/**
 	 * RunGame
 	 */
 	public static void main(String[] args) {
-		Frame MainWindow = new Frame();
+		launch(args);
 	}
 }
